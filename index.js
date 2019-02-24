916ffa8a20e9c3889b9d64e6856810491aa697e1
Huseyin RAN <ranhuseyin@gmail.com>
2019-02-25 00:44:38 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import './src/utils/global';
+import './src/utils/shim';
+
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+AppRegistry.registerComponent('ethereum', () => App);
